19065fd4403a8cbe70e574a060bdf6afaefddf20
hebah al daealieb <hebah20082gmail.com>
2021-09-28 13:23:03 +0300

    exam

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { withAuth0} from "@auth0/auth0-react";
+
+
+
+const axios = require('axios');
 
 class Home extends React.Component {
 
   render() {
     return (
       <>
+
+<Card style={{ width: '18rem' , display: "inline-block"}}>
+  <Card.Img variant="top" src="fruit.image" />
+  <Card.Body>
+    <Card.Title>{fruit.image}</Card.Title>
+    <Card.Text>
+      {/* Some quick example text to build on the card title and make up the bulk of
+      the card's content. */}
+    </Card.Text>
+    <Button variant="primary">Add to Favorite</Button>
+  </Card.Body>
+</Card>
+
         <h1>API Fruits</h1>
       </>
     )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import{ Auth0provider} from"@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
+  <Auth0provider
+  domain=""
+  clientId="" >
+  <App />
+  </Auth0provider>, 
+
   <React.StrictMode>
-    <App />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
